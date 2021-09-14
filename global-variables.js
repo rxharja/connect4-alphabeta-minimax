1e28c68da5ffb3fafdcb0c0942e5c4c046b8f9cd
Redon Xharja <redon.xharja@gmail.com>
2021-09-14 10:48:48 -0400

    created file to hold globals

---
+++ global-variables.js
@@ -0,0 +1,15 @@
+// 0 = yellow, 1 == red
+let player = 0;
+
+// minimax parameters
+let humanPlayer = 0;
+let aiPlayer = 1;
+let maxDepth = 6;
+
+// board parameters
+const gridRows = 6;
+const gridCols = 7;
+
+let grid = Array.from({ length: gridCols }, () => 
+  Array.from({ length: gridRows }, () => -1)
+);
